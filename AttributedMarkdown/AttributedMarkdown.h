d9ff5e38d630881a39417ba05a03d24a92a08300
pavel-zdenek <pavel.zdenek@gmail.com>
2017-02-17 16:33:13 +0100

    Minimal changes for Carthage usability

---
+++ AttributedMarkdown/AttributedMarkdown.h
@@ -0,0 +1,20 @@
+//
+//  AttributedMarkdown.h
+//  AttributedMarkdown
+//
+//  Created by Pavel Zdenek on 11/01/2017.
+//  Copyright © 2017 example.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AttributedMarkdown.
+FOUNDATION_EXPORT double AttributedMarkdownVersionNumber;
+
+//! Project version string for AttributedMarkdown.
+FOUNDATION_EXPORT const unsigned char AttributedMarkdownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AttributedMarkdown/PublicHeader.h>
+#import <AttributedMarkdown/markdown_lib.h>
+#import <AttributedMarkdown/markdown_peg.h>
+
